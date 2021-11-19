77753ec3cabb4404d7f40f80d1a00ff3dd0ca7c1
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-11-19 10:33:36 -0600

    commit tree 137cdbe47d8a1d2a47e9b754d4edca82e6a8510e

--- src/App.js
+++ src/App.js
@@ -34,8 +34,11 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
+  // debugger;
   return { items: state.items }
 }
 
 export default connect(mapStateToProps)(App);
+// * this is okay if we have simple state
+// export default connect((state)=> ({items: state.items}))(App);
+
